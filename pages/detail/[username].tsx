9a07f05a19bd88db421bd329332b40bc076eeafc
irfanca1 <irfanchoiruddin23@gmail.com>
2023-05-26 20:36:21 +0700

    membuat detail user

--- pages/detail/[username].tsx
+++ pages/detail/[username].tsx
@@ -1,9 +1,27 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react';
+import useSWR from 'swr';
+import ProfileHeader from '../../components/ProfileHeader';
+import RepoList from '../../components/RepoList';
+import { UserDetail } from '../../types/UserDetail';
+import fetcher from '../../utils/fetcher';
 
 export default function Detail() {
   const router = useRouter();
   const { username } = router.query;
 
-  return <div>Detail Halaman untuk User : {username}</div>;
+  const { data: user, error } = useSWR<UserDetail>(username && `https://api.github.com/users/${username}`, fetcher);
+
+  return (
+    <div className="py-10">
+      <Head>
+        <title>Detail {user?.name}</title>
+      </Head>
+      <div>
+        <ProfileHeader user={user} />
+        <RepoList reposUrl={user?.repos_url} />
+      </div>
+    </div>
+  );
 }
